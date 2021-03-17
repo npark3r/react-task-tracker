13816e050adb89a091cff477ca3e7d140502f10f
Nick Parker <ncpark3r@gmail.com>
2021-03-17 15:44:44 +1100

    Added json-server functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,10 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={(!showAdd) ? 'green' : 'red'} text={(!showAdd) ? 'Add' : 'Close Add'} onClick={onAdd} />
+      <Button 
+        color={showAdd ? 'red' : 'green'} 
+        text={showAdd ? 'Close' : 'Add'} 
+        onClick={onAdd} />
     </header>
   );
 };
